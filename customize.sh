b76b821d04fd4712b9263b0d7ec9046bd3cd8c7b
coolbele <64392071+coolbele@users.noreply.github.com>
2021-06-06 11:13:28 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -10,5 +10,3 @@ sed -i 's/192.168.1.1/10.0.10.1/g' openwrt/package/base-files/files/bin/config_g
 git clone https://github.com/rufengsuixing/luci-app-adguardhome package/lean/luci-app-adguardhome/
 git clone https://github.com/kenzok8/openwrt-packages.git
 git clone https://github.com/kenzok8/small.git
-./scripts/feeds update -a
-./scripts/feeds install -a
