3bea51d47e0a264c105c0b081bb6450aeeab2dc9
coolbele <64392071+coolbele@users.noreply.github.com>
2021-06-06 10:28:20 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -6,4 +6,9 @@
 # Youtube Channel: https://goo.gl/fvkdwm 
 #=================================================
 #1. Modify default IP
-sed -i 's/192.168.1.1/192.168.5.1/g' openwrt/package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/10.0.10.1/g' openwrt/package/base-files/files/bin/config_generate
+git clone https://github.com/rufengsuixing/luci-app-adguardhome package/lean/luci-app-adguardhome/
+git clone https://github.com/kenzok8/openwrt-packages.git
+git clone https://github.com/kenzok8/small.git
+./scripts/feeds update -a
+./scripts/feeds install -a
